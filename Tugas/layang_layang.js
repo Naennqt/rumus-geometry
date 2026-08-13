2de6ee036ab0049cabc76fb9d2a2b5fdc803fb1d
Naennqt <alnaenn11@gmail.com>
2026-08-13 15:00:14 +0700

    seventh commit

---
+++ Tugas/layang_layang.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")({ sigint: true });
+console.log("------------------------------")
+console.log("Kalkulator LAYANG LAYANG")
+console.log("------------------------------")
+
+let d1 = Number(prompt("Masukkan Panjang Diagonal(cm) 1 :"));
+let d2 = Number(prompt("Masukkan Panjang Diagonal(cm) 2 :"));
+let s1 = Number(prompt("Masukkan Sisi(cm) 1 :"));
+let s2 = Number (prompt("Masukkan Sisi(cm) 2 :"));
+
+let Luas = 1/2 * d1* d2
+let Keliling = 2 * (s1 + s2)
+
+console.log(`Luas     : ${Luas} cm²`);
+console.log(`Keliling     : ${Keliling} cm²`);
